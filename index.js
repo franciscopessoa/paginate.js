95e4dcbed09e752041aa96c95db88c8104660a2f
Alex Alan Nunes <alexalannunes@gmail.com>
2019-03-23 11:24:56 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+const array = [0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 194, 138, 5828, 18, 489, 2398, 981];
+const total = array.length;
+const per_page = 3;
+const total_pages = Math.ceil(total/per_page);
+let init = 0;
+let limit = 3;
+let p_array = [];
+let $div = $('#msg');
+
+function paginate(array, init, limit) {
+  console.log(init, limit)
+  p_array = array.slice(init, limit);
+}
+
+function verify_button_next() {
+  p_array.length !== 3 ? (
+    $('#next').attr('disabled', '')
+  ) : (
+    $('#next').removeAttr('disabled', '')  
+  )
+}
+
+function next() {
+  $div.empty();
+  paginate(array, init, limit);
+  init += 3;
+  limit += 3;
+  show();
+  verify_button_next();
+}
+function previous() {
+  $div.empty();
+  paginate(array, init-3, limit-3);
+  init -= 3;
+  limit -= 3;
+  show();
+
+}
+
+
+function show() {
+  p_array.forEach((item, index) => {
+    $div.append(`<div>${item}</div>`);
+  });
+}
+
+next();
